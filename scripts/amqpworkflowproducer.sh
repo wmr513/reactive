1c929be4f9d3e8dd1dd667e91e31655261b4610b
Mark Richards <mark@wmrichards.com>
2016-09-17 16:03:43 -0400

    Used by Workflow Event Pattern
    Used by Workflow Event Pattern

--- scripts/amqpworkflowproducer.sh
+++ scripts/amqpworkflowproducer.sh
@@ -1,6 +1,6 @@
 HOME=$REACT_HOME
 
-CLASSPATH=$HOME/src/main/resources
+CLASSPATH=$HOME 
 CLASSPATH=$CLASSPATH:$HOME/bin
 CLASSPATH=$CLASSPATH:$HOME/lib/commons-io-1.2.jar
 CLASSPATH=$CLASSPATH:$HOME/lib/rabbitmq-client.jar
