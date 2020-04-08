5c5a68b1df84f562e2dcd2318fa491a31a7de497
Mark Richards <mark@wmrichards.com>
2020-04-08 15:10:07 -0400

    Added code and scripts for the Workflow Event Pattern
    Added code and scripts for the Workflow Event Pattern

---
+++ workflow/java/workflowdelegate.sh
@@ -0,0 +1,10 @@
+HOME=$REACT_HOME
+
+CLASSPATH=$HOME/src/main/resources
+CLASSPATH=$CLASSPATH:$HOME/bin
+CLASSPATH=$CLASSPATH:$HOME/lib/commons-io-1.2.jar
+CLASSPATH=$CLASSPATH:$HOME/lib/rabbitmq-client.jar
+CLASSPATH=$CLASSPATH:$HOME/lib/commons-logging.jar
+CLASSPATH=$CLASSPATH:$HOME/lib/log4j-1.2.17.jar
+
+java -cp $CLASSPATH workflow.AMQPWorkflowProcessor
